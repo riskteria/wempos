00d308be402e954a46087e40cc2efc04bf537f07
riskteria <riskteria@gmail.com>
2015-12-28 12:29:02 +0700

    Pembaharuan Seeder

--- database/seeds/articleTableSeeder.php
+++ database/seeds/articleTableSeeder.php
@@ -19,7 +19,21 @@ class articleTableSeeder extends Seeder
         	'user_id'	=> 1,
         	'title'		=> 'Sekolah Jangan Berkompetisi, tapi Berkolaborasi',
         	'intro'		=> 'Keberadaan sekolah-sekolah yang ada diharapkan tidak saling berkompetisi satu sama lain. Melainkan sama-sama berkolaborasi untuk bisa menciptakan kualitas pendidikan yang lebih baik lagi.',
-        	'content'	=> 'dshhgfrhihfr',
+        	'content'	=> 'JAKARTA - Keberadaan sekolah-sekolah yang ada diharapkan tidak saling berkompetisi satu sama lain. Melainkan sama-sama berkolaborasi untuk bisa menciptakan kualitas pendidikan yang lebih baik lagi.
+
+"Saat ini, bukan lagi zamannya sekolah untuk berkompetensi. Tapi sekolah perlu berkolaborasi," ungkap Ketua Gerakan Sekolah Menyenangkan, Lili Halim di Kedubes Australia, Kamis (26/11/2015).
+Lili mengungkapkan, para siswa yang mengeyam pendidikan di Australia merasakan suasana sekolah yang berbeda di Indonesia.
+
+"Datang ke sekolah dengan berat hati dan pulang dengan senang hati," ujarnya.
+Gerakan ini berusaha untuk mendampingi sekolah-sekolah di Indonesia agar bisa menciptakan suasana yang menyenangkan. Namun, bukan perkara mudah untuk bisa mengubah hal tersebut.
+
+"Menolong sekolah untuk bertransformasi. Pendidikan itu bukan seperti sulap yang bisa langsung berubah. Dan itu cukup panjang prosesnya," ujarnya.
+Dalam gerakan ini juga dikembangkan budaya yang positif. Budaya sehat, baik sehat secara fisik maupun psikis. Menciptakan suasana yang aman dan membuat anak-anak merasa senang.
+
+"Anak-anak kalau bisa merasa senang kalau ketika datang ke sekolah," imbuhnya.
+Selain itu, juga ada partisipasi dari orangtua. Sehingga hasilnya pun bisa maksimal. Serta pendidikan karakter menjadi salah satu poin penting yang diperhatikan.
+
+"Pendidikan karakter itu harus dimulai sejak TK dan SD. Pendidikan karakter menjadi hal yang penting," tambahnya.',
         ]);
 	}
 }
--- readme.md
+++ readme.md
@@ -1,40 +1,24 @@
-# wempos
-
-Perhatikan cara install terlebih dahulu
-
 ##Cara Install
 
-* Install Composer terlebih dahulu
-* Lalu jalankan perintah berikut:
+Download dan Install [Composer](https://getcomposer.org/) terlebih dahulu
 
-    $ Composer Install
+Lalu Ketik perintah berikut:
 
-    $ Composer Update
+    composer install
 
-    gulp
 
------
+    composer update
 
-* ubah exv.example menjadi .env
-* Lalu jalankan perintah berikut:
+Ubah file .env.example mennjadi .env
+
+Jalankan perintah berikut:
 
     php artisan key:generate
 
-    php artisan db:seed
+
+    php artisan migrate --seed
+
 
     php artisan serve
 
------
-
-Buka website dari localhost:8000
-
-
-
-Install dependencies listed in package.json with:
-
-    npm install
-
-Retrieve frontend dependencies with Bower, compile SASS, and move frontend files into place:
-
-    gulp
-
+Buka dan jalankan dari localhost:8000
