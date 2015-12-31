3594d4cf8699deda73c395458ae8eb53e727f10f
riskteria <riskteria@gmail.com>
2015-12-31 13:06:38 +0700

    Tampilan Footer Baru

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,31 +1,41 @@
 <!-- FOOTER SECTION -->
 <footer>
-	<div class="row">
-		<div class="small-12 medium-4 large-4 columns">
-			<p><a href="#" class="title-link">Kebijakan & Privasi</a></p>
-		</div>
+  <div class="row">
+      <div class="small-12 medium-4 large-4 columns">
+          <h5>Informasi</h5>
+          <a href="{{ url('tentang') }}" class="mdc-text-black-darker">Tentang</a><br>
+          <a href="{{ url('kebijakan') }}" class="mdc-text-black-darker">Kebijakan Privasi</a><br>
+          <a href="{{ url('syarat') }}" class="mdc-text-black-darker">Syarat dan Ketentuan</a>
+      </div>
 
-		<div class="small-12 medium-4 large-4 columns">
-			<p><a href="#" class="title-link">Syarat & Ketentuan</a></p>
-		</div>
+      <div class="small-12 medium-4 large-4 columns">
+       <h5>Hubungi Kami</h5>
+          wempos.info@gmail.com<br>
+          Jl. Thamrin Medan Sumatera Utara
+          <br>
+          +(061) 456-789
+          
+      </div>
+      
 
-		<div class="small-12 medium-4 large-4 columns">
-			<p><a href="#" class="title-link">Hubungi Kami</a></p>
-		</div>
-	</div>
-	<!-- END ROW -->
+      <div class="small-12 medium-4 large-4 columns">
+          
+          <h5>Temukan Kami</h5>
+          <a href="http://instagram.com"><i class="fi-social-instagram size-48 mdc-text-black-dark"></i></a>
+          <a href="http://facebook.com"><i class="fi-social-facebook size-48 mdc-text-black-dark"></i></a>
+          <a href="http://twitter.com"><i class="fi-social-twitter size-48 mdc-text-black-dark"></i></a>
+       
+
+      </div>
+  </div>
+  <!-- END ROW -->
 </footer>
-<!-- END FOOTER -->
-
-<!-- COPYRIGHT SECTION -->
 <section class="full-width copyright-section">
-	<div class="row">
-		<div class="small-12 columns">
-			<p>&copy; Website Media Promosi Untuk Sekoah 2014</p>
-		</div>
-		<!-- END 12 COLUMNS -->
-	</div>
-	<!-- END ROW -->
+    <div class="row">
+        <p>&copy; Website Media Promosi Untuk Sekoah 2014</p>
+    </div>
+    
 </section>
 <!-- END SECTION.FULL-WIDTH COPYRIGHT-SECTION -->
 <!-- END OFF CANVAS -->
+<!--Close the off-canvas-->
