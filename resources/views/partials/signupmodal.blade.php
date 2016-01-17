767c59f48b3e2da4148b4f1ed5a04900b9c86b0d
riskteria <riskteria@gmail.com>
2016-01-17 13:21:26 +0700

    Hapus Pendaftaran Pribadi

--- resources/views/partials/signupmodal.blade.php
+++ resources/views/partials/signupmodal.blade.php
@@ -54,32 +54,6 @@
 			</div>
 		</div>
 
-		<div class="large-4 columns">
-			<div class="reveal-card mdc-bg-teal">
-				<div class="reveal-card-image">
-					<img class="img-responsive" src="{{ asset('icon/external/user168.svg') }}">
-
-				</div>
-				<!-- card image -->
-
-				<div class="reveal-card-content clearfix">
-					<span class="reveal-card-title left"><a class="mdc-text-white-dark" href="daftar/pribadi">Pribadi</a></span>
-					<a class="btn btn-custom right mdc-text-white-dark show-card-detail" aria-label="Left Align">
-						<i class="fa fa-ellipsis-v"> </i>
-					</a>
-				</div>
-				<!-- card content -->
-				<div class="reveal-card-reveal">
-					<span class="reveal-card-title">Akun Pribadi</span>
-					<a class="close right" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true" class="fa fa-close"> </span>
-					</a>
-					<p>Here is some more information about this product that is only revealed once clicked on.</p>
-				</div>
-				<!-- card reveal -->
-			</div>
-
-		</div>
 
 	</div>
 	<a class="close-reveal-modal" aria-label="Close">&#215;</a>
